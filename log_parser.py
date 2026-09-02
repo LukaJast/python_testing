49b55a251b07b9a6559e530d3d4f51e162173e2f
Lukasz <luka@lukajast.com>
2026-09-03 00:53:47 +0200

    Initial log parser

---
+++ log_parser.py
@@ -0,0 +1,35 @@
+def count_errors(log_file):
+    """ Function for spotting and counting ERROR in a file"""
+    count = 0
+    # print (log_file)
+    with open(log_file, "r") as file:
+        for line in file:
+            if "ERROR" in line:
+                count += 1
+        return count
+
+
+def has_fatal(log_file):
+    """Checks if the log contain a FATAL error"""
+    fatal = False
+    with open(log_file, "r") as file:
+        for line in file:
+            if "FATAL" in line:
+                fatal = True
+        return fatal
+
+
+
+def find_errors(log_file):
+    """Check line number of first FATAL error"""
+    with open(log_file, "r") as file:
+        for number, line in enumerate(file, start=1):
+            if "ERROR" in line:         
+                error_line = number
+                print(number)
+                return number
+
+# log_file = input("Podaj plik loga: ")
+# number = find_errors(log_file)
+# print("First ERROR is on line " + str(number))
+
---
+++ test_log_parser.py
@@ -0,0 +1,29 @@
+import pytest
+from log_parser import count_errors
+@pytest.mark.parametrize("log_file, expected", [
+    ("log.log", 2),
+    ("clean.log", 0),
+])
+
+def test_count_errors(log_file, expected):
+    assert count_errors(log_file) == expected
+
+from log_parser import has_fatal
+@pytest.mark.parametrize("log_file, expected", [
+    ("log.log", True),
+    ("clean.log", False),
+])
+
+def test_has_fatal(log_file, expected):
+    assert has_fatal(log_file) == expected
+
+from log_parser import find_errors
+@pytest.mark.parametrize ("log_file, expected", [
+    ("log.log", 1),
+    ("clean.log", None),
+])
+
+def test_find_errors(log_file, expected):
+    assert find_errors(log_file) == expected
+
+
