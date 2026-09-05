19e552e0858b6b0bffeb301835c73ce7520f6702
Lukasz <luka@lukajast.com>
2026-09-05 22:32:17 +0200

    Break test intentionally

--- log_parser.py
+++ log_parser.py
@@ -15,7 +15,7 @@ def has_fatal(log_file):
     with open(log_file, "r") as file:
         for line in file:
             if "FATAL" in line:
-                fatal = True
+                fatal = False
         return fatal
 
 
